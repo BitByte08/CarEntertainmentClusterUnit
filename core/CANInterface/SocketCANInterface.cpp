f0761d1a5de90452fa02b9d541185a47253664f5
bitbyte08 <me@bitworkspace.kr>
2025-12-17 10:22:45 +0900

    feat :: Linux용 구현체 생성

---
+++ core/CANInterface/SocketCANInterface.cpp
@@ -0,0 +1,29 @@
+#include "SocketCANInterface.hpp"
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <stdexcept>
+
+SocketCANInterface::SocketCANInterface(const std::string &interface_name) {
+    socket_fd_ = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (socket_fd_ < 0) throw std::runtime_error("Failed to create CAN socket");
+    
+    struct ifreq ifr{};
+    std::strncpy(ifr.ifr_name, interface_name.c_str(), IFNAMSIZ -1);
+    if(ioctl(socket_fd_, SIOCGIFINDEX, &ifr) < 0) {
+        close(socket_fd_);
+        throw std::runtime_error("Failed to get interface index for " + interface_name);
+    }
+
+    struct sockaddr_can addr{};
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+    if (bind(socket_fd_, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        close(socket_fd_);
+        throw std::runtime_error("Failed to bind CAN socket to interface " + interface_name);
+    }
+}
