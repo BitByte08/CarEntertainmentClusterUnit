7d6c168e82db86e82e6ed2ff8df813c41cf66fcf
bitbyte08 <me@bitworkspace.kr>
2025-12-17 10:12:26 +0900

    feat :: 구현체 분리

---
+++ core/CANInterface/SocketCANInterface.hpp
@@ -0,0 +1,15 @@
+#ifndef CORE_SOCKETCANINTERFACE_CPP
+#define CORE_SOCKETCANINTERFACE_CPP
+#include "CANInterface.hpp"
+
+class SocketCANInterface : public CANInterface {
+public:
+    explicit SocketCANInterface(const std::string &interface_name);
+    ~SocketCANInterface();
+
+    bool recive(can_frame &frame) override;
+private:
+    int socket_fd_{-1};
+};
+
+#endif // CORE_SOCKETCANINTERFACE_CPP
