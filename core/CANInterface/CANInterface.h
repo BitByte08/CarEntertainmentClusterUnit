6dd9a435b8d9308d9da104e10de62b276204486a
bitbyte08 <me@bitworkspace.kr>
2025-12-17 10:02:24 +0900

    feat :: 헤더 정의

--- core/CANInterface/CANInterface.h
+++ core/CANInterface/CANInterface.h
@@ -1,4 +1,6 @@
 #ifndef CORE_CANINTERFACE_H
+#define CORE_CANINTERFACE_H
+
 #include <linux/can.h>
 #include <linux/can/raw.h>
 #include <sys/socket.h>
@@ -6,11 +8,13 @@
 #include <cstring>
 #include <stdexcept>
 #include <string>
-#define CORE_CANINTERFACE_H
+
+struct can_frame;
 
 class CANInterface {
 public:
-private:
+    virtual ~CANInterface() = default;
+    virtual bool recive(can_frame &frame) = 0;
 };
 
 #endif // CORE_CANINTERFACE_H
