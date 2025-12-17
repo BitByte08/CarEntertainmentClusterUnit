2b29568f1457ab61cbbaf664bd026edde210067b
bitbyte08 <me@bitworkspace.kr>
2025-12-17 09:47:20 +0900

    feat :: include header

---
+++ core/CANInterface/CANInterface.h
@@ -0,0 +1,16 @@
+#ifndef CORE_CANINTERFACE_H
+#include <linux/can.h>
+#include <linux/can/raw.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <cstring>
+#include <stdexcept>
+#include <string>
+#define CORE_CANINTERFACE_H
+
+class CANInterface {
+public:
+private:
+};
+
+#endif // CORE_CANINTERFACE_H
